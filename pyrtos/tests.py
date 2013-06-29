348bdacdb7824d301631181b4ce592b81103753d
Alexander Skjolden <alex@plastboks.net>
2013-06-29 12:05:01 +0200

    comments #1 - test usermodel
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- pyrtos/tests.py
+++ pyrtos/tests.py
@@ -6,7 +6,7 @@ from cryptacular.bcrypt import BCRYPTPasswordManager as BPM
 
 from .models import DBSession
 
-def _initTestingDB():
+def _initTestingDB(makeuser=False):
     from sqlalchemy import create_engine
     from pyrtos.models import (
         DBSession,
@@ -16,16 +16,46 @@ def _initTestingDB():
     engine = create_engine('sqlite://')
     Base.metadata.create_all(engine)
     DBSession.configure(bind=engine)
-    m = BPM()
-    hashed = m.encode(u'abc123')
-    with transaction.manager:
-        user = User(
-                        username=u'user1',
-                        email=u'user1@local',
-                        password=hashed
-                    )
-        DBSession.add(user)
+    if makeuser:
+      m = BPM()
+      hashed = m.encode(u'abc123')
+      with transaction.manager:
+          user = User(
+                          username=u'user1',
+                          email=u'user1@local',
+                          password=hashed
+                      )
+          DBSession.add(user)
     return DBSession
+
+
+class UserModelTests(unittest.TestCase):
+    def setUp(self):
+        self.session = _initTestingDB(makeuser=False)
+
+    def tearDown(self):
+        self.session.remove()
+
+    def _getTargetClass(self):
+        from pyrtos.models import User
+        return User
+
+    def _makeOne(self, username, email, password):
+        m = BPM()
+        hashed = m.encode(password)
+        return self._getTargetClass()(username=username,
+                                      email=email,
+                                      password=hashed)
+
+    def test_constructor(self):
+        m = BPM()
+        instance = self._makeOne(username='user',
+                            email='user@email.com',
+                            password='1234')
+        self.assertEqual(instance.username, 'user')
+        self.assertEqual(instance.email, 'user@email.com')
+        self.assertTrue(m.check(instance.password, '1234'))
+
 
 class ViewTests(unittest.TestCase):
     def setUp(self):
@@ -49,6 +79,7 @@ class ViewTests(unittest.TestCase):
         response = index(request)
         self.assertEqual(response['title'], 'Hello world')
 
+
 class FunctionlTests(unittest.TestCase):
     def setUp(self):
         from pyrtos import main
@@ -56,13 +87,18 @@ class FunctionlTests(unittest.TestCase):
         app = main({}, **settings)
         from webtest import TestApp
         self.testapp = TestApp(app)
-        _initTestingDB()
+        _initTestingDB(makeuser=True)
 
     def tearDown(self):
         del self.testapp
         from pyrtos.models import DBSession
         DBSession.remove()
 
-    def test_it(self):
+    def test_root(self):
         res = self.testapp.get('/', status=200)
         self.assertIn(b'Hello world', res.body)
+
+    def test_login(self):
+        res = self.testapp.get('/login', status=200)
+        self.assertIn(b'Login', res.body)
+
