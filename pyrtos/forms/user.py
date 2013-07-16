e64f2d182518c44d9b4135323ff141974f35885b
Alexander Skjolden <alex@plastboks.net>
2013-07-16 14:00:45 +0200

    comments #30 - pep8 forms
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- pyrtos/forms/user.py
+++ pyrtos/forms/user.py
@@ -11,34 +11,37 @@ from wtforms import (
 
 from pyrtos.models import User
 
+
 class UserCreateForm(BaseForm):
     email = TextField('Email',
                       [validators.Length(max=255),
                        validators.Email(message='Not an valid email address')],
                       filters=[strip_filter])
     givenname = TextField('Givenname',
-                      [validators.Length(max=255)],
-                      filters=[strip_filter])
+                          [validators.Length(max=255)],
+                          filters=[strip_filter])
     surname = TextField('Surname',
-                      [validators.Length(max=255)],
-                      filters=[strip_filter])
+                        [validators.Length(max=255)],
+                        filters=[strip_filter])
     password = PasswordField('Password',
-                            [validators.Length(min=6, max=128),
-                             validators.EqualTo('confirm',
-                                                message='Passwords must match')],
-                            filters=[strip_filter])
+                             [validators.Length(min=6, max=128),
+                              validators.EqualTo('confirm',
+                                                 message='Passwords\
+                                                          must match')],
+                             filters=[strip_filter])
     confirm = PasswordField('Confirm password',
                             filters=[strip_filter])
     group = SelectField('Group',
-                        choices=[(n,n) for n in User.groups])
+                        choices=[(n, n) for n in User.groups])
     blocked = BooleanField('Blocked')
 
 
 class UserEditForm(UserCreateForm):
     password = PasswordField('Password',
-                             [validators.Optional(), 
+                             [validators.Optional(),
                               validators.Length(min=6, max=128),
                               validators.EqualTo('confirm',
-                                                  message='Passwords must match')],
-                              filters=[strip_filter])
+                                                 message='Passwords\
+                                                          must match')],
+                             filters=[strip_filter])
     id = HiddenField()
