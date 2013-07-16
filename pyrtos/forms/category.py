1c586467055750e77cff2bada853ef086c5053b0
Alexander Skjolden <alex@plastboks.net>
2013-07-16 13:49:41 +0200

    comments #30 - pep8 valid
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- pyrtos/forms/category.py
+++ pyrtos/forms/category.py
@@ -8,11 +8,13 @@ from wtforms import (
     BooleanField,
 )
 
+
 class CategoryCreateForm(BaseForm):
     title = TextField('Category Title',
                       [validators.Length(min=3, max=255)],
                       filters=[strip_filter])
     private = BooleanField('Private', default=False)
+
 
 class CategoryEditForm(CategoryCreateForm):
     id = HiddenField()
