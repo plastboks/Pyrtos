08729b9212435e912553571015fdda5cd7456c93
Alexander Skjolden <alex@plastboks.net>
2013-07-16 15:18:14 +0200

    comments #30 - pep auth, category, creditor and expenditures views
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- pyrtos/views/category.py
+++ pyrtos/views/category.py
@@ -25,32 +25,31 @@ from pyrtos.forms import (
     CategoryEditForm,
 )
 
+
 class CategoryViews(object):
 
-    def __init__(self,request):
+    def __init__(self, request):
         self.request = request
-
 
     @view_config(route_name='categories',
                  renderer='pyrtos:templates/category/list.mako',
                  permission='view')
     def categories(self):
-        page = int (self.request.params.get('page', 1))
+        page = int(self.request.params.get('page', 1))
         categories = Category.page(self.request, page)
         return {'paginator': categories,
-                'title' : 'Categories',
-                'archived' : False}
+                'title': 'Categories',
+                'archived': False}
 
     @view_config(route_name='categories_archived',
                  renderer='pyrtos:templates/category/list.mako',
                  permission='view')
     def categories_archived(self):
-        page = int (self.request.params.get('page', 1))
+        page = int(self.request.params.get('page', 1))
         categories = Category.page(self.request, page, archived=True)
         return {'paginator': categories,
-                'title' : 'Archived categories',
-                'archived' : True,}
-
+                'title': 'Archived categories',
+                'archived': True}
 
     @view_config(route_name='category_new',
                  renderer='pyrtos:templates/category/edit.mako',
@@ -64,13 +63,12 @@ class CategoryViews(object):
             form.populate_obj(c)
             c.user_id = authenticated_userid(self.request)
             DBSession.add(c)
-            self.request.session.flash('Category %s created' %\
-                                            (c.title), 'success')
+            self.request.session.flash('Category %s created' %
+                                       (c.title), 'success')
             return HTTPFound(location=self.request.route_url('categories'))
         return {'title': 'New category',
                 'form': form,
                 'action': 'category_new'}
-
 
     @view_config(route_name='category_edit',
                  renderer='pyrtos:templates/category/edit.mako',
@@ -89,14 +87,13 @@ class CategoryViews(object):
 
         if self.request.method == 'POST' and form.validate():
             form.populate_obj(c)
-            self.request.session.flash('Category %s updated' %\
-                                            (c.title), 'status')
+            self.request.session.flash('Category %s updated' %
+                                       (c.title), 'status')
             return HTTPFound(location=self.request.route_url('categories'))
-        return {'title' : 'Edit category',
-                'form' : form,
-                'id' : id,
-                'action' : 'category_edit'}
-
+        return {'title': 'Edit category',
+                'form': form,
+                'id': id,
+                'action': 'category_edit'}
 
     @view_config(route_name='category_archive',
                  renderer='string',
@@ -112,10 +109,9 @@ class CategoryViews(object):
 
         c.archived = True
         DBSession.add(c)
-        self.request.session.flash('Category %s archived' %\
-                                        (c.title), 'status')
+        self.request.session.flash('Category %s archived' %
+                                   (c.title), 'status')
         return HTTPFound(location=self.request.route_url('categories'))
-
 
     @view_config(route_name='category_restore',
                  renderer='string',
@@ -131,7 +127,7 @@ class CategoryViews(object):
 
         c.archived = False
         DBSession.add(c)
-        self.request.session.flash('Category %s restored' %\
-                                        (c.title), 'status')
-        return HTTPFound(location=self.request.route_url('categories_archived'))
-
+        self.request.session.flash('Category %s restored' %
+                                   (c.title), 'status')
+        return HTTPFound(location=self.request
+                                      .route_url('categories_archived'))
