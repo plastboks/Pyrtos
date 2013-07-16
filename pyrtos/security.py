c13eb74fd02e542d2d955f3719a765f66be29a0e
Alexander Skjolden <alex@plastboks.net>
2013-07-16 13:46:02 +0200

    comments #30 - pep8 valid
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- pyrtos/security.py
+++ pyrtos/security.py
@@ -10,6 +10,7 @@ from pyramid.security import (
 
 from pyrtos.models import User
 
+
 class EntryFactory(object):
     __name__ = None
     __parent__ = None
@@ -18,7 +19,7 @@ class EntryFactory(object):
                (Allow, Authenticated, 'edit'),
                (Allow, 'group:admin', 'delete'),
                (Allow, 'group:admin', 'archive'),
-               (Allow, 'group:admin', 'restore'),]
+               (Allow, 'group:admin', 'restore'), ]
 
     def __init__(self, request):
         pass
@@ -28,6 +29,7 @@ def groupfinder(userid, request):
     user = User.by_id(userid)
     group = user.group
     return ['group:'+group]
+
 
 def can_i(request, perm):
     return has_permission(perm, request.context, request)
