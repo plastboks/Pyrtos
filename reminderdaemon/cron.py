047c93c7bd8c8e7c67e846a612a4618b5d263a61
Alexander Skjolden <alex@plastboks.net>
2013-08-28 11:27:35 +0200

    comments #45 - some sceleton files
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

---
+++ reminderdaemon/cron.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#
+# This is the crondaemon for Pyrtos. This library class is
+# responsible for running trough the database and create
+# reminders.
+#
+#
+
+class Cron():
+    
+    __init__():
+        # this class should extend or inherit a SqlAlchemy instance.
+
