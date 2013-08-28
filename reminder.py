7930c84107419c7a08c24cc157c49fba355e8244
Alexander Skjolden <alex@plastboks.net>
2013-08-28 15:40:47 +0200

    comments #45 - fixed none working paste config loader
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- reminder.py
+++ reminder.py
@@ -9,12 +9,16 @@ __requires__ = 'pyramid==1.4.2'
 import sys
 from pkg_resources import load_entry_point
 
-from paste.deploy.loadwsgi import ConfigLoader
+from paste.deploy import appconfig
 from reminderdaemon import Cron
 
 if not len(sys.argv) is 2:
     print('You must specify a .ini file')
     sys.exit()
 
-f = sys.argv[1]
-c = ConfigLoader(f)
+try:
+    f = sys.argv[1]
+    c = appconfig('config:'+f, relative_to=".")
+except Exception as e:
+    print(e)
+    sys.exit()
